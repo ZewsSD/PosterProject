759c530bcc6260ad09e4d242c8695192dea53133
ZewsSD <76225337+ZewsSD@users.noreply.github.com>
2022-06-07 14:39:59 +0300

    Add files via upload

--- Poster/Model/DBModels/City.cs
+++ Poster/Model/DBModels/City.cs
@@ -1,16 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 #nullable disable
 
 namespace Poster.Model.DBModels
 {
-    public partial class City
+    public partial class City : IReadOnlyCity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int? CinemaId { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int? CinemaId { get; private set; }
 
-        public virtual Cinema Cinema { get; set; }
+        public virtual Cinema Cinema { get; private set; }
+
+        public City(string name, int cinemaId)
+        {
+            Name = name;
+            CinemaId = cinemaId;
+        }
+
+        public void Update(string name, int cinemaId)
+        {
+            if (name != null)
+                Name = name;
+            if (cinemaId != -1)
+                CinemaId = cinemaId;
+        }
+
+        public void AddCinema(TimeSpan workTime, string title, string address)
+        {
+            Cinema cinema = new Cinema(workTime, title, address);
+
+            using (PosterDbContext db = new PosterDbContext())
+            {
+                db.Cinemas.Add(cinema);
+                db.SaveChanges();
+            }
+        }
+
+        public void RemoveCinema(int id)
+        {
+            using (PosterDbContext db = new PosterDbContext())
+            {
+                Cinema cinema = db.Cinemas.FirstOrDefault(x => x.Id == id);
+
+                if (cinema != null)
+                {
+                    db.Cinemas.Remove(cinema);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public void UpdateCinema(int id, TimeSpan workTime = new TimeSpan(), string title = null, string address = null)
+        {
+            using (PosterDbContext db = new PosterDbContext())
+            {
+                Cinema cinema = db.Cinemas.FirstOrDefault(i => i.Id == id);
+
+                cinema.Update(workTime, title, address);
+
+                db.Cinemas.Update(cinema);
+                db.SaveChanges();
+            }
+        }
+
+        public IReadOnlyList<IReadOnlyCinema> GetAllCinemas()
+        {
+            using (PosterDbContext db = new PosterDbContext())
+            {
+                return db.Cinemas.ToList();
+            }
+        }
+
+        public IReadOnlyCinema GetCinema(int id)
+        {
+            using (PosterDbContext db = new PosterDbContext())
+            {
+                return db.Cinemas.FirstOrDefault(x => x.Id == id);
+            }
+        }
+    }
+
+    public interface IReadOnlyCity
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int? CinemaId { get; }
     }
 }
--- Poster/Model/DBModels/Session.cs
+++ Poster/Model/DBModels/Session.cs
@@ -5,20 +5,42 @@ using System.Collections.Generic;
 
 namespace Poster.Model.DBModels
 {
-    public partial class Session
+    public partial class Session : IReadOnlySession
     {
-        public Session()
+        public int Id { get; private set; }
+        public int? MovieId { get; private set; }
+        public DateTime? Date { get; private set; }
+        public int? HallId { get; private set; }
+
+        public virtual Hall Hall { get; private set; }
+        public virtual Movie Movie { get; private  set; }
+        public virtual ICollection<Ticket> Tickets { get; private set; }
+
+        public Session(DateTime date, int movieId, int hallId)
         {
+            Date = date;
+            MovieId = movieId;
+            HallId = hallId;
+
             Tickets = new HashSet<Ticket>();
         }
 
-        public int Id { get; set; }
-        public int? MovieId { get; set; }
-        public DateTime? Date { get; set; }
-        public int? HallId { get; set; }
+        public void Update(DateTime date, int movieId, int hallId)
+        {
+            if (date != null)
+                Date = date;
+            if (movieId != -1)
+                MovieId = movieId;
+            if (hallId != -1)
+                HallId = hallId;
+        }
+    }
 
-        public virtual Hall Hall { get; set; }
-        public virtual Movie Movie { get; set; }
-        public virtual ICollection<Ticket> Tickets { get; set; }
+    public interface IReadOnlySession
+    {
+        public int Id { get; }
+        public int? MovieId { get; }
+        public DateTime? Date { get; }
+        public int? HallId { get; }
     }
 }
