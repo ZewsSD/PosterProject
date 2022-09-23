374f960e174f8fbd57243b248570b30c748fc92d
ZewsSD <76225337+ZewsSD@users.noreply.github.com>
2022-09-23 20:58:04 +0300

    Add files via upload

---
+++ ViewModel/TicketViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Poster.ViewModel
+{
+    class TicketViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
