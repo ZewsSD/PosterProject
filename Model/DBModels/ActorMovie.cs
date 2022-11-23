4ea1637d7d99565965286dc7bf0c020c96fb67d8
ZewsSD <76225337+ZewsSD@users.noreply.github.com>
2022-11-23 18:26:25 +0300

    Add files via upload

---
+++ Model/DBModels/ActorMovie.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+#nullable disable
+
+namespace Poster.Model.DBModels
+{
+    public partial class ActorMovie : /*IReadOnlyActorMovie,*/ INotifyPropertyChanged
+    {
+        private int _id;
+        private int? _actorId;
+        private int? _movieId;
+        private Actor _actor;
+        private Movie _movie;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        public int? ActorId
+        {
+            get => _actorId;
+            set
+            {
+                _actorId = value;
+                OnPropertyChanged(nameof(ActorId));
+            }
+        }
+        public int? MovieId
+        {
+            get => _movieId;
+            set
+            {
+                _movieId = value;
+                OnPropertyChanged(nameof(MovieId));
+            }
+        }
+
+        public virtual Actor Actor
+        {
+            get => _actor;
+            set
+            {
+                _actor = value;
+                OnPropertyChanged(nameof(Actor));
+            }
+        }
+        public virtual Movie Movie
+        {
+            get => _movie;
+            set
+            {
+                _movie = value;
+                OnPropertyChanged(nameof(Movie));
+            }
+        }
+
+        //public ActorMovie(int? actor_id, int? movie_id, Actor actor, Movie movie)
+        //{
+        //    ActorId = actor_id;
+        //    MovieId = movie_id;
+
+        //    Actor = actor;
+        //    Movie = movie;
+        //}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = " ") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public void Update(int actor_id, int movie_id)
+        {
+            if (actor_id != -1)
+                this.ActorId = actor_id;
+            if (movie_id != -1)
+                this.MovieId = movie_id;
+        }
+    }
+
+    public interface IReadOnlyActorMovie
+    {
+        public int Id { get; }
+        public int? ActorId { get; }
+        public int? MovieId { get; }
+    }
+}
